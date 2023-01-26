9b663fe85236972591aae149f292587752021136
Andrade Christian <andrade.dias.christian@gmail.com>
2023-01-25 22:04:14 -0300

    instance spaceships with identificator

---
+++ station.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const TypesSpaceships_1 = require("./TypesSpaceships");
+const spitBloom = new TypesSpaceships_1.FireSpaceship('Spit Bloom Fire Base');
+const lifePod = new TypesSpaceships_1.GrassSpaceship('Grass Time LifePOD');
+const oceanNew = new TypesSpaceships_1.WaterSpaceship('Super Nova Ocean');
---
+++ station.ts
@@ -0,0 +1,5 @@
+import { FireSpaceship, GrassSpaceship, WaterSpaceship } from "./TypesSpaceships";
+
+const spitBloom = new FireSpaceship('Spit Bloom Fire Base')
+const lifePod = new GrassSpaceship('Grass Time LifePOD')
+const oceanNew = new WaterSpaceship('Super Nova Ocean')
