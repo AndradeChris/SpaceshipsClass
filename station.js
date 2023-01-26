d6a0c57e135125a2945106b80810c6b3f505303b
Andrade Christian <andrade.dias.christian@gmail.com>
2023-01-25 22:06:38 -0300

    remove js files

--- station.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TypesSpaceships_1 = require("./TypesSpaceships");
-const spitBloom = new TypesSpaceships_1.FireSpaceship('Spit Bloom Fire Base');
-const lifePod = new TypesSpaceships_1.GrassSpaceship('Grass Time LifePOD');
-const oceanNew = new TypesSpaceships_1.WaterSpaceship('Super Nova Ocean');
